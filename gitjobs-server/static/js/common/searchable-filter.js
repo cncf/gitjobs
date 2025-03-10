a0d635e61e708d3a81bff6c048725e17b61e29e8
Cintia Sánchez García <cynthiasg@icloud.com>
2025-03-10 21:47:23 +0100

    Improve mobile view
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- gitjobs-server/static/js/common/searchable-filter.js
+++ gitjobs-server/static/js/common/searchable-filter.js
@@ -4,7 +4,6 @@ import { triggerChangeOnForm } from "/static/js/jobboard/filters.js";
 
 export class SearchableFilter extends LitElement {
   static properties = {
-    title: { type: String },
     name: { type: String },
     options: { type: Array },
     selected: { type: Array },
@@ -17,7 +16,6 @@ export class SearchableFilter extends LitElement {
 
   constructor() {
     super();
-    this.title = "";
     this.name = "name";
     this.options = [];
     this.selected = [];
@@ -54,15 +52,16 @@ export class SearchableFilter extends LitElement {
   async cleanSelected() {
     this.selected = [];
 
-     // Wait for the update to complete
-     await this.updateComplete;
+    // Wait for the update to complete
+    await this.updateComplete;
   }
 
   filterOptions() {
     if (this.enteredValue.length > 0) {
-      this.visibleOptions = this.options.filter((option) =>
-        option.toLowerCase().includes(this.enteredValue.toLowerCase()),
-      );
+      this.visibleOptions = this.options.filter((option) => {
+        const name = this.name === "projects" ? option.name : option;
+        return name.toLowerCase().includes(this.enteredValue.toLowerCase());
+      });
     } else {
       this.visibleOptions = this.options;
     }
@@ -134,24 +133,46 @@ export class SearchableFilter extends LitElement {
           ${this.visibleOptions.length > 0
             ? html`<ul class="py-2 text-sm text-gray-700 overflow-auto max-h-[180px]">
                 ${this.visibleOptions.map((option) => {
-                  const isSelected = this.selected.includes(option);
+                  const isProjectsType = this.name === "projects";
+                  const name = isProjectsType ? option.name : option;
+                  const isSelected = this.selected.includes(name);
                   return html`<li class="group" data-index="{{ loop.index }}">
                     <button
                       type="button"
-                      @click=${() => this._onSelect(option)}
+                      @click=${() => this._onSelect(name)}
                       class=${`${
                         isSelected ? "bg-gray-100 opacity-50" : "cursor-pointer hover:bg-gray-100"
-                      } capitalize block w-full text-left px-4 py-2`}
+                      } capitalize block w-full text-left px-${isProjectsType ? "3" : "4"} py-2`}
                       ?disabled="${isSelected}"
                     >
-                      <div class="flex items-center">
-                        <div class="size-3 me-2">
-                          ${isSelected
-                            ? html`<div class="svg-icon size-3 icon-check bg-gray-400"></div>`
-                            : ""}
-                        </div>
-                        <div class="truncate">${unnormalize(option)}</div>
-                      </div>
+                      ${isProjectsType
+                        ? html`<div class="flex items-center space-x-3">
+                            <img
+                              class="size-10"
+                              height="40"
+                              width="40"
+                              src="${option.logo_url}"
+                              alt="${name} logo"
+                            />
+                            <div class="flex flex-col justify-start min-w-0">
+                              <div class="truncate text-start text-gray-700 font-medium">${name}</div>
+                              <div class="inline-flex">
+                                <div
+                                  class="truncate text-nowrap uppercase max-w-[100%] text-xs/6 font-medium text-gray-400"
+                                >
+                                  CNCF ${option.maturity}
+                                </div>
+                              </div>
+                            </div>
+                          </div>`
+                        : html`<div class="flex items-center">
+                            <div class="size-3 me-2">
+                              ${isSelected
+                                ? html`<div class="svg-icon size-3 icon-check bg-gray-400"></div>`
+                                : ""}
+                            </div>
+                            <div class="truncate">${unnormalize(name)}</div>
+                          </div>`}
                     </button>
                   </li>`;
                 })}
@@ -165,11 +186,15 @@ export class SearchableFilter extends LitElement {
               (opt) => html` <button
                   type="button"
                   @click=${() => this._onRemove(opt)}
-                  class="inline-flex items-center justify-between px-2 py-1 bg-white border rounded-lg cursor-pointer select-none border-primary-500 text-primary-500 group"
+                  class="inline-flex items-center justify-between px-2 py-1 bg-white border rounded-lg cursor-pointer select-none border-primary-500 text-primary-500 max-w-full group"
                 >
                   <div class="flex items-center justify-between space-x-3 w-full">
-                    <div class="text-[0.8rem] text-center text-nowrap capitalize">${unnormalize(opt)}</div>
-                    <div class="svg-icon size-3 icon-close bg-gray-500 group-hover:bg-gray-800"></div>
+                    <div class="text-[0.8rem] text-center text-nowrap capitalize truncate">
+                      ${unnormalize(opt)}
+                    </div>
+                    <div
+                      class="svg-icon size-3 icon-close bg-gray-500 group-hover:bg-gray-800 shrink-0"
+                    ></div>
                   </div>
                 </button>
                 <input type="hidden" name="${this.name}[]" value="${opt}" />`,
--- gitjobs-server/static/js/jobboard/filters.js
+++ gitjobs-server/static/js/jobboard/filters.js
@@ -3,6 +3,7 @@ export const open = () => {
   const drawer = document.getElementById("drawer-filters");
   drawer.classList.add("transition-transform");
   drawer.classList.remove("-translate-x-full");
+  drawer.dataset.open = "true";
   const backdrop = document.getElementById("drawer-backdrop");
   backdrop.classList.remove("hidden");
 };
@@ -12,6 +13,7 @@ export const close = () => {
   const drawer = document.getElementById("drawer-filters");
   drawer.classList.add("-translate-x-full");
   drawer.classList.remove("transition-transform");
+  drawer.dataset.open = "false";
   const backdrop = document.getElementById("drawer-backdrop");
   backdrop.classList.add("hidden");
 };

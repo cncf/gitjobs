f042ee6d22b0de8bb3e359a0cd7981fab8ed4c4b
Sergio Castaño Arteaga <tegioz@icloud.com>
2025-03-20 12:36:50 +0100

    Add location filter
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- gitjobs-server/src/handlers/misc.rs
+++ gitjobs-server/src/handlers/misc.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use anyhow::Result;
 use askama::Template;
 use axum::{
+    Json,
     extract::{Query, State},
     http::StatusCode,
     response::{Html, IntoResponse},
@@ -27,12 +28,11 @@ pub(crate) async fn search_locations(
     Query(query): Query<HashMap<String, String>>,
 ) -> Result<impl IntoResponse, HandlerError> {
     let Some(ts_query) = query.get("ts_query_location") else {
-        return Ok((StatusCode::BAD_REQUEST, "missing ts_query parameter").into_response());
+        return Ok((StatusCode::BAD_REQUEST, "missing ts_query_location parameter").into_response());
     };
     let locations = db.search_locations(ts_query).await?;
-    let template = misc::Locations { locations };
 
-    Ok(Html(template.render()?).into_response())
+    Ok(Json(locations).into_response())
 }
 
 /// Handler that returns the members search results.

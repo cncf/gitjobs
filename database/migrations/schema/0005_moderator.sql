204fdcdf224321378c32326687392c484c477d33
Sergio Castaño Arteaga <tegioz@icloud.com>
2025-04-09 15:12:26 +0200

    Consolidate database migrations
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/schema/0005_moderator.sql
+++
@@ -1,5 +0,0 @@
-alter table "user" add moderator boolean not null default false;
-
----- create above / drop below ----
-
-alter table "user" drop column moderator;
--- database/migrations/schema/0006_moderator.sql
+++
@@ -1,7 +0,0 @@
-insert into job_status (name) values ('pending-approval');
-insert into job_status (name) values ('rejected');
-
----- create above / drop below ----
-
-delete from job_status where name = 'pending-approval';
-delete from job_status where name = 'rejected';

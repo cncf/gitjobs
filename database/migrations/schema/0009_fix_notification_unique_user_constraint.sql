a7f196b1f58d200944c12b892a39d66dd2490b43
Sergio Castaño Arteaga <tegioz@icloud.com>
2025-05-13 09:53:48 +0200

    Add new notification kind
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/schema/0009_fix_notification_unique_user_constraint.sql
+++
@@ -1,5 +0,0 @@
-alter table notification drop constraint notification_user_id_key;
-
----- create above / drop below ----
-
-alter table notification add constraint notification_user_id_key unique (user_id);
